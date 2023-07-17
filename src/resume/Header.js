55f62b9903914c0c2b20167708a9374647e4942a
Jayden <jpylesbusiness@gmail.com>
2023-07-16 22:30:11 -0500

    feat: add work experience

--- src/resume/Header.js
+++ src/resume/Header.js
@@ -1,8 +1,9 @@
 function Header() {
   return (
-    <>
-      <h1 className="title">Jayden Pyles</h1>
-    </>
+    <div className="title">
+      <h1>Jayden Pyles</h1>
+      <h4>Software Developer</h4>
+    </div>
   );
 }
 export default Header;
