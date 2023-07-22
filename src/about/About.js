733936e6e282422c1b724b364775c64307f131eb
Jayden Pyles <jpylesbusiness@gmail.com>
2023-07-22 11:39:50 -0500

    feat: add about section

--- src/about/About.js
+++ src/about/About.js
@@ -9,8 +9,21 @@ const About = () => {
         <div className="about text">
           <h2>About </h2>
           <p>
-            About me!
-            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            I am a Computer Science student at the University of Alabama at
+            Birmingham. I'm currently a Software Developer and IT Specialist at
+            the{" "}
+            <a
+              href="https://www.uab.edu/cas/home/research/areas-of-expertise/computing-cyber-security"
+              className="italic link"
+            >
+              UAB Computer Forensics Research Lab
+            </a>
+            . I also have around two years of experience in OSINT Analysis from
+            my time working as a Cybercrime Analyst in the lab.
+          </p>
+          <p>
+            I currently work most with React, Python, Postgres SQL, Docker, and
+            other full-stack development tools/technologies.
           </p>
         </div>
       </div>
