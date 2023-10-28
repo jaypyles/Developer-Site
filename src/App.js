a7335ee96ca705df1240eb24bd6885676d375a4a
Jayden <jpylesbusiness@gmail.com>
2023-10-28 14:19:50 -0500

    fix: broken image path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
+import Website from "./common/WEBSITE.md";
+import Nav from "react-bootstrap/Nav";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <PageComponent />
+    </div>
+  );
+}
 
 const PageComponent = () => {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    fetch("WEBSITE.md")
+    fetch(Website)
       .then((res) => res.text())
       .then((text) => setContent(text));
   }, []);
@@ -16,11 +45,3 @@ const PageComponent = () => {
     </div>
   );
 };
-
-export default function App() {
-  return (
-    <div className="App">
-      <PageComponent />
-    </div>
-  );
-}
