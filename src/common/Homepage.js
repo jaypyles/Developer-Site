8dd4543f03203252f2829271f2554e095f67fd84
Jayden Pyles <jpylesbusiness@gmail.com>
2023-12-19 20:13:25 -0600

    chore: update homepage

--- src/common/Homepage.js
+++ src/common/Homepage.js
@@ -35,19 +35,20 @@ const Homepage = () => {
           but those will get explained in the
           <a href="https://wiki.jaydenpyles.dev/books/network-setup">setup</a>
           page. In terms of webserver applications, it also hosts my own cloud
-          storage with filebrowser
+          storage with nextcloud
           <label for="wiki2">2</label>
           <input type="checkbox" id="wiki2" />
           <small>
-            <a href="https://github.com/filebrowser/filebrowser">filebrowser</a>
-            provides a file managing interface within a specified directory and
-            it can be used to upload, delete, preview, rename and edit your
-            files. It allows the creation of multiple users and each user can
-            have its own directory. It can be used as a standalone app.
+            <a href="https://github.com/nextcloud">nextcloud</a>
+            Nextcloud Office is a powerful LibreOffice-based online office suite
+            with collaborative editing, which supports all major document,
+            spreadsheet and presentation file formats and works in all modern
+            browsers.
           </small>
           which is running in a Docker container and can be found at
-          <a href="https://files.jaydenpyles.dev">files.jaydenpyles.dev</a>. My
-          wiki hosted via Bookstack
+          <a href="https://files.jaydenpyles.dev">files.jaydenpyles.dev</a> (but
+          not publicly accessbile, only through tailscale can it be accessed).
+          My wiki hosted via Bookstack
           <label for="wiki3">3</label>
           <input type="checkbox" id="wiki3" />
           <small>
