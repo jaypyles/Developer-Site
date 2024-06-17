cef241189726eb9c9e9765067652be59a89fd66d
Jayden Pyles <jpylesbusiness@gmail.com>
2024-06-17 17:18:28 -0500

    fix: edit mocks name

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,8 @@ const jestConfig = {
   },
   moduleNameMapper: {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/mocks/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/mocks/fileMock.js",
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js",
   },
   testEnvironment: "jsdom",
 };
