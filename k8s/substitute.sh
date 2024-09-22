9e26131075d3ae88de22b0e75a8a9ea56d9053a9
Jayden Pyles <jpylesbusiness@gmail.com>
2024-09-22 13:51:04 -0500

    fix: subsitute script

--- k8s/substitute.sh
+++ k8s/substitute.sh
@@ -4,7 +4,7 @@
 export BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 # Read args from .env file and export them into the environment
-export $(grep -v '^#' .env.dev | xargs)
+export $(grep -v '^#' .env | xargs)
 
 # Substitute variables from templates into manifests and overwrite the result directly to the target file
 envsubst < k8s/templates/frontend-configmap.template.yaml > k8s/manifests/frontend-configmap.yaml
