dd913178593b5b612a672892cb7478c54053d96a
Jayden Pyles <111098627+jaypyles@users.noreply.github.com>
2024-09-22 13:29:47 -0500

    feat: migrate to k3s (#7)
    * wip: add comment
    
    * wip: add k8s infra

---
+++ k8s/substitute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get current branch name
+export BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+# Read args from .env file and export them into the environment
+export $(grep -v '^#' .env.dev | xargs)
+
+# Substitute variables from templates into manifests and overwrite the result directly to the target file
+envsubst < k8s/templates/frontend-configmap.template.yaml > k8s/manifests/frontend-configmap.yaml
+envsubst < k8s/templates/frontend-deployment.template.yaml > k8s/manifests/frontend-deployment.yaml
+
+# Print the output to verify if needed
+cat k8s/manifests/frontend-configmap.yaml
